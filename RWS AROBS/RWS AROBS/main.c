25c3ec1b2bdd48d754bd8729d65ca65d9f366853
Bruno Sebesi <bruno.sebesi@intern.arobs.com>
2018-07-10 08:18:01 -0700

    1st day

--- RWS AROBS/RWS AROBS/main.c
+++ RWS AROBS/RWS AROBS/main.c
@@ -6,13 +6,35 @@
  */ 
 
 #include <avr/io.h>
+#define SETPORTB(PIN) PORTB|=(1<<(PIN))
+#define CLEARPORTB(PIN) PORTB &=~ (1<<(PIN))
 
+typedef enum{
+	APort = 0,
+	BPort,
+	CPort,
+	DPort,
+	EPort,
+	FPort } EN_Port_Type;
+	
+
+void CONTROLPIN(unsigned char pinNr, unsigned char pinState, EN_Port_Type Port2Control);
 
 int main(void)
 {
+	DDRB = 0x06;
+	SETPORTB(1);
+	SETPORTB(2);
+	CONTROLPIN(3,0,APort);
     /* Replace with your application code */
     while (1) 
-    {
-    }
+		  
+    {	  
+		CLEARPORTB(1);
+    }	  
 }
 
+void CONTROLPIN(unsigned char pinNr, unsigned char pinState, EN_Port_Type Port2Control)
+{
+	
+}
