4edb6907fe81d8a98edfce8a955316fb69bcc671
Bruno Sebesi <bruno.sebesi@intern.arobs.com>
2018-07-10 23:04:12 -0700

    2nd day for cycle

--- RWS AROBS/RWS AROBS/main.c
+++ RWS AROBS/RWS AROBS/main.c
@@ -4,11 +4,15 @@
  * Created: 7/10/2018 6:36:57 AM
  * Author : bruno.sebesi
  */ 
-
+#define F_CPU 1000000
 #include <avr/io.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <util/delay.h>
 #define SETPORTB(PIN) PORTB|=(1<<(PIN))
 #define CLEARPORTB(PIN) PORTB &=~ (1<<(PIN))
-
+#define SETPORTC(PIN) PORTC|=(1<<(PIN))
+#define CLEARPORTC(PIN) PORTC &=~(1<<PIN)
 typedef enum{
 	APort = 0,
 	BPort,
@@ -18,23 +22,56 @@ typedef enum{
 	FPort } EN_Port_Type;
 	
 
-void CONTROLPIN(unsigned char pinNr, unsigned char pinState, EN_Port_Type Port2Control);
+void ControlPin(EN_Port_Type Port2Control, uint8_t pinNr, bool pinState);
 
 int main(void)
 {
-	DDRB = 0x06;
-	SETPORTB(1);
-	SETPORTB(2);
-	CONTROLPIN(3,0,APort);
+	static bool pinstate = true;
+	unsigned char i; 
+//	DDRB = 0x02;
+	DDRC = 0x3F;
+	
     /* Replace with your application code */
     while (1) 
 		  
     {	  
-		CLEARPORTB(1);
+/*
+		ControlPin(BPort,1,pinstate);
+		ControlPin(CPort,4,pinstate);
+		_delay_ms(1000);
+		pinstate^=1;*/
+	
+	for (i=0; i<=5; i++)
+	{
+		ControlPin(CPort,i,pinstate);
+		_delay_ms(1000);
+	}
+		pinstate^=1;
     }	  
 }
 
-void CONTROLPIN(unsigned char pinNr, unsigned char pinState, EN_Port_Type Port2Control)
+void ControlPin(EN_Port_Type Port2Control, uint8_t pinNr, bool pinState)
 {
+	static uint16_t counter = 0;
+	counter++;
+	switch(Port2Control) {
+		case (BPort):
+			if (pinState) {
+				SETPORTB(pinNr);
+			} else {
+				CLEARPORTB(pinNr);
+			}
+		break;
+		case(CPort):
+			if(pinState){
+				SETPORTC(pinNr);
+			} else {
+				CLEARPORTC(pinNr);	
+			}
+		break;
+		default:
+		break;
+	}
+		
 	
 }
